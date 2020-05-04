34e2b617b1618a6d5e2933517bdfea9f5b809328
vasenkov_ivan <swayket@yandex.ru>
2020-05-04 14:56:34 +0300

    переделал формы

--- index-send.php
+++ index-send.php
@@ -1,49 +1,13 @@
 <?php
-// Файлы phpmailer ВСЕ ЧТО НИЖЕ, до следующего коммента НУЖНО ДЛЯ ТОГО ЧТО БЫ ПУШИТЬ БДБДБДБДБДБДДБДБ
-// Файлы phpmailer ВСЕ ЧТО НИЖЕ, до следующего коммента НУЖНО ДЛЯ ТОГО ЧТО БЫ ПУШИТЬ БДБДБДБДБДБДДБДБ
-// Файлы phpmailer ВСЕ ЧТО НИЖЕ, до следующего коммента НУЖНО ДЛЯ ТОГО ЧТО БЫ ПУШИТЬ БДБДБДБДБДБДДБДБ
-// if (
-//     isset($_POST['surname']) &&
-//     isset($_POST['name']) &&
-//     isset($_POST['patronymic']) &&
-//     isset($_POST['address']) &&
-//     isset($_POST['tel'])
-// ) {
-
-//     // Параметры для подключения
-//     $db_host = "localhost";
-//     $db_user = "id13203941_admin"; // Логин БД
-//     $db_password = "DfyzDfctyrjd777@"; // Пароль БД
-//     $db_base = 'id13203941_customers'; // Имя БД
-//     $db_table = "clients"; // Имя Таблицы БД
-
-//     // Подключение к базе данных
-//     $mysqli = new mysqli($db_host, $db_user, $db_password, $db_base);
-
-//     $result = $mysqli->query("INSERT INTO " . $db_table . " (surname,name,patronymic,address,tel) VALUES ('$surname','$name','$patronymic','$address','$tel')");
-
-//     if ($result == true) {
-//         echo " <h1> <b>Спасибо</b>, данные отправлены!</h1>  </br>  <button> <a style='margin: 1em; font-size: 2em' href='/index.html'>Вернуться на сайт</a> </button>";
-//     } else {
-//         echo "Упс, что то пошло не так ;(";
-//     }
-// }
-// Файлы phpmailer ВСЕ ЧТО НИЖЕ, до следующего коммента НУЖНО ДЛЯ ТОГО ЧТО БЫ ПУШИТЬ БДБДБДБДБДБДДБДБ
-//  Файлы phpmailer ВСЕ ЧТО НИЖЕ, до следующего коммента НУЖНО ДЛЯ ТОГО ЧТО БЫ ПУШИТЬ БДБДБДБДБДБДДБДБ
-// Файлы phpmailer ВСЕ ЧТО НИЖЕ, до следующего коммента НУЖНО ДЛЯ ТОГО ЧТО БЫ ПУШИТЬ БДБДБДБДБДБДДБДБ
-
-
 // Файлы phpmailer ВСЕ ЧТО НИЖЕ, НУЖНО ДЛЯ ТОГО ЧТО БЫ ПУШИТЬ ПОЧТУ
 require 'phpmailer/PHPMailer.php';
 require 'phpmailer/SMTP.php';
 require 'phpmailer/Exception.php';
-
 // Переменные, которые отправляет пользователь
-$surname = $_POST['surname'];
 $name = $_POST['name'];
-$patronymic = $_POST['patronymic'];
-$address = $_POST['address'];
+$email = $_POST['email'];
 $tel = $_POST['tel'];
+$text = $_POST['text'];
 
 $mail = new PHPMailer\PHPMailer\PHPMailer();
 
@@ -64,14 +28,12 @@ try {
     // Само письмо
     $mail->isHTML(true);
 
-    $mail->Subject = 'Добавлен новый клиент';
-    $mail->Body    = "
-        <h1>Новый клиент: </h1><br>
-        <b>Фамилия: </b> $surname <br>
-        <b>Имя: </b> $name <br>
-        <b>Отчество: </b> $patronymic <br>
-        <b>Адрес: </b> $address <br>
-        <b>Телефон: </b> $tel";
+    $mail->Subject = 'Новая заявка (перезвонить)';
+    $mail->Body    = "<b>Имя:</b> $name <br>
+        <b>Почта:</b> $email <br>
+        <b>Телефон:</b> $tel <br>
+        <br>
+        <b>Сообщение:</b><br>$text";
 
 
     // Проверяем отравленность сообщения
@@ -85,22 +47,19 @@ try {
     // Файлы phpmailer ВСЕ ЧТО НИЖЕ, до следующего коммента НУЖНО ДЛЯ ТОГО ЧТО БЫ ПУШИТЬ БДБДБДБДБДБДДБДБ
     // Файлы phpmailer ВСЕ ЧТО НИЖЕ, до следующего коммента НУЖНО ДЛЯ ТОГО ЧТО БЫ ПУШИТЬ БДБДБДБДБДБДДБДБ
     // Файлы phpmailer ВСЕ ЧТО НИЖЕ, до следующего коммента НУЖНО ДЛЯ ТОГО ЧТО БЫ ПУШИТЬ БДБДБДБДБДБДДБДБ
-
-    if (isset($_POST['surname']) && isset($_POST['name'])  && isset($_POST['patronymic']) && isset($_POST['address']) && isset($_POST['tel'])) {
-
-
+    if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['tel']) && isset($_POST['text'])) {
 
         // Параметры для подключения
         $db_host = "localhost";
         $db_user = "id13203941_admin"; // Логин БД
         $db_password = "DfyzDfctyrjd777@"; // Пароль БД
         $db_base = 'id13203941_customers'; // Имя БД
-        $db_table = "clients"; // Имя Таблицы БД
+        $db_table = "mytable"; // Имя Таблицы БД
 
         // Подключение к базе данных
         $mysqli = new mysqli($db_host, $db_user, $db_password, $db_base);
 
-        $result = $mysqli->query("INSERT INTO " . $db_table . " (surname,name,patronymic,address,tel) VALUES ('$surname','$name','$patronymic','$address','$tel')");
+        $result = $mysqli->query("INSERT INTO " . $db_table . " (name,email,tel,text) VALUES ('$name','$email','$tel','$text')");
 
         if ($result == true) {
             echo " <h1> <b>Спасибо</b>, данные отправлены!</h1>  </br>  <button> <a style='margin: 1em; font-size: 2em' href='/index.html'>Вернуться на сайт</a> </button>";
